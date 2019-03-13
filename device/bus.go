dc138cf5f550da1201587451bb2dba0dce928279
jTRUCO <9823050+jtruco@users.noreply.github.com>
2019-03-13 19:23:56 +0100

    device : organize bus

---
+++ device/bus.go
@@ -0,0 +1,38 @@
+package device
+
+import "github.com/jtruco/emu8/cpu"
+
+// -----------------------------------------------------------------------------
+// Bus device
+// -----------------------------------------------------------------------------
+
+// Bus event types
+const (
+	// Access is a bus access event
+	BusAccess = 10
+	// Read is a bus read event
+	BusRead = 11
+	// Write is a bus write event
+	BusWrite = 12
+)
+
+// Bus is the device databus interface
+type Bus interface {
+	// Device interface
+	Device
+	// DataBus interface
+	cpu.DataBus
+}
+
+// BusEvent is a bus event
+type BusEvent struct {
+	// Is a device event
+	Event
+	// Address on bus
+	Address uint16
+}
+
+// BusListener is a bus event listener
+type BusListener interface {
+	ListenBusEvent(event *BusEvent)
+}
--- device/device.go
+++ device/device.go
@@ -1,8 +1,6 @@
 // Package device contains common device components like memory, video, audio, io, etc.
 package device
 
-import "github.com/jtruco/emu8/cpu"
-
 // -----------------------------------------------------------------------------
 // Device
 // -----------------------------------------------------------------------------
@@ -44,39 +42,4 @@ type Event struct {
 // Listener is a device event listener
 type Listener interface {
 	ListenDeviceEvent(event *Event)
-}
-
-// -----------------------------------------------------------------------------
-// Bus device
-// -----------------------------------------------------------------------------
-
-// Bus event types
-const (
-	// Access is a bus access event
-	BusAccess = 10
-	// Read is a bus read event
-	BusRead = 11
-	// Write is a bus write event
-	BusWrite = 12
-)
-
-// Bus is the device databus interface
-type Bus interface {
-	// Device interface
-	Device
-	// DataBus interface
-	cpu.DataBus
-}
-
-// BusEvent is a bus event
-type BusEvent struct {
-	// Is a device event
-	Event
-	// Address on bus
-	Address uint16
-}
-
-// BusListener is a bus event listener
-type BusListener interface {
-	ListenBusEvent(event *BusEvent)
 }
