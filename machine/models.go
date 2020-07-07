8ed1df75759fba0d785e12f4944c3390983b7035
jTRUCO <9823050+jtruco@users.noreply.github.com>
2020-07-07 17:34:13 +0200

    machine : include zx80 / zx81 models

--- machine/models.go
+++ machine/models.go
@@ -11,6 +11,7 @@ const (
 	UnknownMachine = iota
 	ZXSpectrum
 	AmstradCPC
+	ZX8081      // NOT IMPLEMENTED
 	Commodore64 // NOT IMPLEMENTED
 	MSX         // NOT IMPLEMENTED
 )
@@ -21,6 +22,8 @@ const (
 	ZXSpectrum16k
 	ZXSpectrum48k
 	AmstradCPC464
+	ZX80         // NOT IMPLEMENTED
+	ZX81         // NOT IMPLEMENTED
 	CommodoreC64 // NOT IMPLEMENTED
 	MSX1         // NOT IMPLEMENTED
 )
@@ -32,6 +35,8 @@ const DefaultModel = ZXSpectrum48k
 var Machines = map[string]int{
 	"zxspectrum":  ZXSpectrum,
 	"amstradcpc":  AmstradCPC,
+	"zx80":        ZX8081,
+	"zx81":        ZX8081,
 	"commodore64": Commodore64,
 	"msx":         MSX,
 }
@@ -48,12 +53,16 @@ func GetMachine(name string) int {
 
 // Models machine model name mapping
 var Models = map[string]int{
-	"zx16k":  ZXSpectrum16k,
-	"zx48k":  ZXSpectrum48k,
-	"speccy": ZXSpectrum48k,
-	"cpc464": AmstradCPC464,
-	"c64":    CommodoreC64,
-	"msx1":   MSX1,
+	"zxspectrum16k": ZXSpectrum16k,
+	"zx16k":         ZXSpectrum16k,
+	"zxspectrum48k": ZXSpectrum48k,
+	"zx48k":         ZXSpectrum48k,
+	"speccy":        ZXSpectrum48k,
+	"zx80":          ZX80,
+	"zx81":          ZX81,
+	"cpc464":        AmstradCPC464,
+	"c64":           CommodoreC64,
+	"msx1":          MSX1,
 }
 
 // GetModel gets model ID from name
@@ -70,6 +79,7 @@ func GetModel(name string) int {
 var MachineModels = map[int][]int{
 	ZXSpectrum:  {ZXSpectrum16k, ZXSpectrum48k},
 	AmstradCPC:  {AmstradCPC464},
+	ZX8081:      {ZX80, ZX81},
 	Commodore64: {CommodoreC64},
 	MSX:         {MSX1},
 }
